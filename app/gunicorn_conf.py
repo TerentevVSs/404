1494ab7dff2da7757440f1405912fd21075a3d2a
Sergey Natalenko <sergey.natalenko@mail.ru>
2022-06-13 06:52:22 +0300

    Update

--- app/gunicorn_conf.py
+++ app/gunicorn_conf.py
@@ -4,3 +4,4 @@ bind = '0.0.0.0:' + environ.get('APP_PORT', '8080')
 max_requests = 1000
 worker_class = 'uvicorn.workers.UvicornWorker'
 workers = int(environ.get('APP_WORKERS', 1))
+worker_timeout = 600
